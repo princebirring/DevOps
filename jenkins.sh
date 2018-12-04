fab5a774c35ed93d8f00a8143fea50ea872208c9
princebirring <princebirring1992@gmail.com>
2018-12-04 18:53:47 -0500

    jenkins installation shell script

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
+sudo ufw allow 8080
+sudo ufw status
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
