a15812f6fc7626bd4eb5c75aecbb44235078a007
Prince Birring <princebirring1992@gmail.com>
2018-12-11 11:12:21 -0500

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,10 @@
 #Installing Jenkins in Ubuntu
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jdk -y 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y 
+sudo apt-get install jenkins -y 
 sudo ufw allow 8080
 sudo ufw status
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
